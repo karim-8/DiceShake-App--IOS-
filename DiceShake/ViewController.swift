db6dc5a396d573cf2e233a5073d192fe50dd3fa6
Karim <karim@192.168.118.128>
2018-03-31 01:49:59 -0700

    Initial Commit

---
+++ DiceShake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiceShake
+//
+//  Created by Karim on 7/22/1734 ERA1.
+//  Copyright © 1734 ERA1 Kariim8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
