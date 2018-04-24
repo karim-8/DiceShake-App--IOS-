31960ae7f90f399cfa5ca87db77ccf6cdd8b252b
Karim <karim@192.168.118.128>
2018-04-24 08:08:36 -0700

    Intial Commit

--- DiceShake/ViewController.swift
+++ DiceShake/ViewController.swift
@@ -1,25 +1,48 @@
-//
-//  ViewController.swift
-//  DiceShake
-//
-//  Created by Karim on 7/22/1734 ERA1.
-//  Copyright © 1734 ERA1 Kariim8. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    var indeximage1 = 0
+    var indexImage2 = 0
+    
+    @IBOutlet weak var imageView1: UIImageView!
+    
+    @IBOutlet weak var imageView2: UIImageView!
+   
+let diceArray = //images names
+    ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    func updateImage()  {
+        //generating random numbers from 1...6
+        indeximage1 = Int(arc4random_uniform(6))
+        indexImage2 = Int(arc4random_uniform(6))
+        
+        //change images upon the random values
+        imageView1.image = UIImage(named: diceArray[indeximage1])
+        imageView2.image = UIImage(named: diceArray[indexImage2])
+    }
+    
+    @IBAction func didButtonPressed(_ sender: Any) {
+        updateImage()
+    }
+    //when device shaked
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake{
+         updateImage()
+            
+        }
+    }
+    
 
 }
 
